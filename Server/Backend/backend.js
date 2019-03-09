9721a23355973230d31ebdf9ad3d0923687f14c8
EinfachAlex <33007348+EinfachAlex@users.noreply.github.com>
2019-03-09 11:52:31 +0100

    sending donation to server

---
+++ Server/Backend/backend.js
@@ -0,0 +1,30 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const crypto = require("crypto");
+const db = require("better-sqlite3")("./../../donations.db");
+const config = require("./../../config.json");
+var app = express();
+
+let urlencodedParser = bodyParser.urlencoded({
+    extended: true
+});
+
+app.use(function (req, res, next) {
+    res.header('Access-Control-Allow-Origin', '*')
+    res.header('Access-Control-Allow-Headers', '*');
+    next()
+})
+
+app.post('/login', urlencodedParser, function (req, res) {
+
+    console.log(req.body);
+
+    var nimiqMsg = crypto.createHmac("sha256", (JSON.stringify(req.body) + " " + Date.now()).toString()).digest('hex');
+    var kiddy = db.prepare("INSERT INTO donations (nimiqmsg, user, amount, timestamp) VALUES (?, ?, ?, ?)").run(nimiqMsg, req.body.user, req.body.amount, Date.now());
+    
+    res.send(`https://safe.nimiq.com/#/_new-transaction/${config.address}/recipient/${req.body.amount}/${nimiqMsg}_`);
+}); 
+
+app.listen(3000, function () {
+    console.log("Started on PORT 3000");
+});
