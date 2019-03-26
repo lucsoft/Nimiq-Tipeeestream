c14fe4b759299ffe0d4dff182ba07c71c1000588
EinfachAlex <33007348+EinfachAlex@users.noreply.github.com>
2019-03-26 20:57:04 +0100

    setup -> add tipeeestream api key if not added

---
+++ Client/setup/setup.js
@@ -0,0 +1,10 @@
+const fs = require('fs');
+const config = require("./../config.json");
+
+function submit() {
+    config.tipeeeapikey = document.getElementsByName("tipeeeapikey")[0].value;
+
+    fs.writeFileSync("./config.json", JSON.stringify(config, null, 4));
+}
+
+document.getElementById('submit').addEventListener('click', submit);
