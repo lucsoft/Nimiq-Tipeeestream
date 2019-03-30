98af4208100d5cc318d13eb3f0e1e61c8aee4040
EinfachAlex <33007348+EinfachAlex@users.noreply.github.com>
2019-03-30 12:32:06 +0100

    add nimiqxkey and nimiqadress via settings page

--- Client/settings/settings.js
+++ Client/settings/settings.js
@@ -4,16 +4,38 @@ const shell = require("electron").shell;
 
 function submit() {
     config.tipeeeapikey = document.getElementsByName("tipeeeapikey")[0].value;
+    config.nimiqxapikey = document.getElementsByName("nimiqxapikey")[0].value;
+    config.address = document.getElementsByName("address")[0].value;
 
     fs.writeFileSync("./config.json", JSON.stringify(config, null, 4));
 
     document.location.href = "./../home/index.html";
 }
 
-function openTipeee() {
-    shell.openExternal("https://www.tipeeestream.com/dashboard/api-key");
+function openTab(link) {
+    switch (link) {
+        case "tipeee":
+            shell.openExternal("https://www.tipeeestream.com/dashboard/api-key");
+            break;
+        case "nimiqx":
+            shell.openExternal("https://account.nimiqx.com/keys");
+            break;
+        case "safe":
+            shell.openExternal("https://safe.nimiq.com");
+            break;
+    }
 }
 
 document.getElementById('submit').addEventListener('click', submit);
 
-document.getElementById('tipeeelink').addEventListener('click', openTipeee);
+document.getElementById('tipeeelink').addEventListener('click', function () {
+    openTab("tipeee");
+});
+
+document.getElementById('nimiqxlink').addEventListener('click', function () {
+    openTab("nimiqx");
+});
+
+document.getElementById('safelink').addEventListener('click', function () {
+    openTab("safe");
+});
